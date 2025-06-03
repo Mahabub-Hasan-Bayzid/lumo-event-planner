6767f8749bae22c287a4e660806297e33ecc24de
Md Saifuddullah <sdtutulctg@gmail.com>
2025-06-03 15:31:15 +0300

    main-navigation-added

--- resources/js/components/Header.jsx
+++ resources/js/components/Header.jsx
@@ -38,7 +38,9 @@ function Header() {
                             </li>
                         </ul>
                     </div>
-                    <a className="btn btn-ghost text-xl">daisyUI</a>
+                    <a className="btn btn-ghost text-xl">
+                        Lumo <span className="text-emerald-500">Event</span>
+                    </a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
